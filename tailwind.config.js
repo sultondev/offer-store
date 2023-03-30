268f2eb7da82f26b57be21a4b9e6fbca643177f6
sultondev <sultondeveloper@gmail.com>
2023-03-31 00:42:55 +0500

    fix: deleted unused built in codes and added fonts also styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      josefin: ["Josefin Sans", "sans-serif"],
+      lato: ["Lato", "sans-serif"]
+    },
     extend: {},
   },
   plugins: [],
